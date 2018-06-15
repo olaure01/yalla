b88ebe4c671c703174909915a8798138f8898110
Olivier Laurent <olivier.laurent@ens-lyon.fr>
2018-06-15 13:37:15 +0200

    updated file dependency

--- mylibs/mylibs.mk
+++ mylibs/mylibs.mk
@@ -1,5 +1,5 @@
 $(MYLIBSDIR)/AFC.vo: $(MYLIBSDIR)/AFC.v
-$(MYLIBSDIR)/Bool_more.vo: $(MYLIBSDIR)/Bool_more.v
+$(MYLIBSDIR)/Bool_more.vo: $(MYLIBSDIR)/Bool_more.v $(MYLIBSDIR)/List_Type.vo
 $(MYLIBSDIR)/CPermutation_solve.vo: $(MYLIBSDIR)/CPermutation_solve.v $(MYLIBSDIR)/List_more.vo $(MYLIBSDIR)/CyclicPerm.vo
 $(MYLIBSDIR)/CyclicPerm.vo: $(MYLIBSDIR)/CyclicPerm.v $(MYLIBSDIR)/List_more.vo $(MYLIBSDIR)/Permutation_more.vo
 $(MYLIBSDIR)/fmsetlist.vo : $(MYLIBSDIR)/fmsetlist.v $(MYLIBSDIR)/Bool_more.vo $(MYLIBSDIR)/Injective.vo
